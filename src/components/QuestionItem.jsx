963f3db0ff5ec996753d6f7ab80ea12e7f35f6df
puneet56 <puneetdhiman56@gmail.com>
2021-11-10 18:45:04 +0530

    Feature:Difficulty Rating

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import tw from 'tailwind-styled-components';
+import { AiFillStar } from 'react-icons/ai';
 
 const Container = tw.h1`w-full flex-grow bg-gray-400 flex flex-col items-center justify-start
 `;
@@ -9,15 +10,20 @@ const Category = tw.p`pt-4 text-lg text-gray-800 px-10 w-full
 `;
 const OptionsContainer = tw.div`flex flex-row items-center justify-center max-w-xl w-full flex-wrap
 `;
-const Option = tw.button` w-[30%] border-2 border-gray-900 bg-gray-700 text-white rounded-xl px-4 py-2 md:m-10 m-3 my-5
+const Option = tw.button` w-[30%] border-2 border-gray-900 bg-gray-700 text-white rounded-xl px-4 py-2 md:m-5 m-3 my-5
 `;
-const Next = tw.button`w-[40%] absolute bottom-10 border-2 border-gray-900 bg-gray-700 text-white text-xl rounded-xl px-8 py-4 m-10
+const Next = tw.button`w-[40%] absolute bottom-10 border-2 border-gray-900 bg-gray-700 text-white text-xl rounded-xl px-4 py-2 m-10
+`;
+const StarDiv = tw.div`flex flex-row items-center justify-start h-10 w-full pl-10
 `;
 
 const QuestionItem = ({ item, next, correct, last }) => {
 	const [selected, setSelected] = useState(false);
 	const [answered, setAnswered] = useState(false);
 	const [alert, setAlert] = useState('');
+
+	const difficulty =
+		item.difficulty === 'hard' ? 3 : item.difficulty === 'medium' ? 2 : 1;
 
 	const options = useMemo(
 		() =>
@@ -50,6 +56,11 @@ const QuestionItem = ({ item, next, correct, last }) => {
 		<Container>
 			<QuestionText>{decodeURIComponent(item.question)}</QuestionText>
 			<Category>{decodeURIComponent(item.category)}</Category>
+			<StarDiv>
+				<AiFillStar />
+				<AiFillStar className={difficulty >= 2 ? '' : 'text-gray-500'} />
+				<AiFillStar className={difficulty >= 3 ? '' : 'text-gray-500'} />
+			</StarDiv>
 			<OptionsContainer>
 				{options.map((option, index) => (
 					<Option
@@ -68,6 +79,9 @@ const QuestionItem = ({ item, next, correct, last }) => {
 					</Option>
 				))}
 			</OptionsContainer>
+			{last && (
+				<QuestionText className='text-center'>Last Question</QuestionText>
+			)}
 			{answered && <QuestionText className='text-center'>{alert}</QuestionText>}
 			{!last && answered && <Next onClick={handleNext}>Next</Next>}
 		</Container>
